d1f53c539921714048c3ad38e5f0035bab94e37c
Vasilii Turtugeshev <70282280+FuR1DeV@users.noreply.github.com>
2023-03-08 17:05:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 let tg = window.Telegram.WebApp;
 
-tg.expand()
+tg.expand();
 
-document.getElementById('send').onclick = myClick;
-
-function myClick() {
-    Telegram.WebApp.onEvent('send', function() {
-        tg.sendData(document.getElementById('autocomplete').value)
-    })
+window.onload=function(){
+    new google.maps.places.Autocomplete((document.getElementById("autocomplete")), {
+    types:'geocode'})
+    tg.MainButton.show()
+    tg.MainButton.enable()
 }
+
+Telegram.WebApp.onEvent('mainButtonClicked', function() {
+    console.log('inside!')
+    tg.sendData(document.getElementById('autocomplete').value)
+    tg.close()
+})
